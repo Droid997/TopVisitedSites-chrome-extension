369505dce05efd890ebbe07d8d06201520f6915c
Droid997 <dishanth1997@gmail.com>
2018-12-29 19:26:22 +0530

    made lighter and faster

--- popup.js
+++ popup.js
@@ -1,22 +1,20 @@
 
-$( document ).ready(function() {
-	var value="dishanth";
-	chrome.storage.sync.get(['key'], function(result) {
-          for(var i=0;i<result.key.array.length;i++)
-			{
-					var tr = $('<tr class="no-margin">');
-					var a=$('<a style="font-size: small;">').text(result.key.array[i].title);
-					a.attr("href",result.key.array[i].url);
-					a.click(function(data){
-		
-						var tag=data.currentTarget;
-						console.log(tag.href);
-						chrome.tabs.create({url:tag.href});
-					});
-					tr.append($('<td>').append(a));
-					$('#key_table').append(tr);
-			}
-        });
-	
-	
-});
+function setTopSites(topSites)
+{
+	for(var i=0;i<topSites.length;i++)
+	{
+			var tr = $('<tr class="no-margin">');
+			var a=$('<a style="font-size: small;">').text(topSites[i].title);
+			a.attr("href",topSites[i].url);
+			a.click(function(data){
+
+				var tag=data.currentTarget;
+				console.log(tag.href);
+				chrome.tabs.create({url:tag.href});
+			});
+			tr.append($('<td>').append(a));
+			$('#key_table').append(tr);
+	}
+}
+
+chrome.topSites.get(setTopSites);
